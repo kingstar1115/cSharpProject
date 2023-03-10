428a884f31fa47945f9d9464a954e293e9341ebe
kingstar1115 <hatoyama1115@gmail.com>
2023-03-11 05:35:14 +0900

    C# project start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace csharp_test
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(2 + 2);
+
+        }
+    }
+}
